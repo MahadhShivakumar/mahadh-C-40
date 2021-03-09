981f6410002889e5b4cd65d0ecb910029bf968fc
unknown <maduexplosive@gmail.com>
2021-03-09 14:59:05 +0530

    a

--- sketch.js
+++ sketch.js
@@ -14,11 +14,11 @@ var track, car1_img, car2_img, car3_img, car4_img;
 
 function preload(){
   track = loadImage("../images/track.jpg");
-  car1_img = loadImage("../images/car1.png");
-  car2_img = loadImage("../images/car2.png");
-  car3_img = loadImage("../images/car3.png");
-  car4_img = loadImage("../images/car4.png");
-  ground = loadImage("../images/ground.png");
+  car1_img = loadImage("./images/car1.png");
+  car2_img = loadImage("./images/car2.png");
+  car3_img = loadImage("./images/car3.png");
+  car4_img = loadImage("./images/car4.png");
+  ground = loadImage("./images/ground.png");
 }
 
 function setup(){
@@ -27,6 +27,7 @@ function setup(){
   game = new Game();
   game.getState();
   game.start();
+  
 }
 
 
@@ -41,4 +42,5 @@ function draw(){
   if(gameState === 2){
     game.end();
   }
+
 }
